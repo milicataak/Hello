78b6dc58926ee3a6266b3735e279372cbd1820d9
ADM\az58427 <miltom@aarhus.dk>
2021-10-05 12:26:30 +0200

    Starting

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello again!");
+
+            Console.ReadLine();
+        }
+    }
+}
